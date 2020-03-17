089bc10d683bf29661c711729a6a74c85c691e1c
Thomas A Johansen <thomasaj@gmail.com>
2020-03-17 10:00:31 +0100

    Initial commit

---
+++ 100 Percent Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  100 Percent Calculator
+//
+//  Created by Thomas Andre Johansen on 17/03/2020.
+//  Copyright © 2020 Appkokeriet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
