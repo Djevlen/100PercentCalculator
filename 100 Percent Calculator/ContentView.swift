211852e0774675c55650d4eb378f0e00ce90b347
Thomas A Johansen <thomasaj@gmail.com>
2020-03-17 12:25:31 +0100

    Configuring Tab Bar

--- 100 Percent Calculator/ContentView.swift
+++ 100 Percent Calculator/ContentView.swift
@@ -9,8 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    #warning("get selectedTab from user settings, favs by default")
+    @State private var selectedTab = "Calculations"
+    
     var body: some View {
-        Text("Hello, World!")
+        TabView(selection: $selectedTab){
+            
+            Text("Calculations")
+                .tabItem{
+                    Image(systemName: "percent")
+                    Text("Calculations")
+            }
+            .tag("Calculations")
+            
+            Text("Favorites")
+                .tabItem{
+                    Image(systemName: "star.fill")
+                    Text("Favorites")
+            }
+            .tag("Favorites")
+            
+            Text("Settings")
+                .tabItem{
+                    Image(systemName: "gear")
+                    Text("Settings")
+            }
+            .tag("Settings")
+        }
+    }
+}
+
+//extension to configure appearance
+extension UITabBarController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let standardAppearance = UITabBarAppearance()
+        
+        // update the appearance
+        
+        
+        tabBar.standardAppearance = standardAppearance
     }
 }
 
