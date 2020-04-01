c1257bad28d6e2dff47281af2b7fe7715c57eddf
Thomas A Johansen <thomasaj@gmail.com>
2020-04-01 10:52:11 +0200

    Updated privacy policy

--- 100 Percent Calculator/Views/Settings/AboutView.swift
+++ 100 Percent Calculator/Views/Settings/AboutView.swift
@@ -18,7 +18,7 @@ struct AboutView: View {
             Image(systemName: "person")
             Text("Thank you for downloading it - I hope it helps you out! 😊")
             Text("")
-            Text("I (try to) make a living by making apps, so I'd be really thankful if you'd consider buying PRO, or maybe leaving a positive review by pressing the button below.")
+            Text("I (try to) make a living by making apps, and I decided to remove all ads from 100%, so I'd be really thankful if you'd consider buying PRO, giving me a tip, or maybe leaving a positive review by pressing the button below.")
                 .font(.body)
             Text("")
             Text("- Thomas ❤️")
--- 100 Percent Calculator/Views/Settings/PrivacyPolicyView.swift
+++ 100 Percent Calculator/Views/Settings/PrivacyPolicyView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 
 struct PrivacyPolicyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+                Text("Privacy is very important to me, and is taken very seriously. In this app I do not collect any of your identifiable data, nor sell, rent or give away any information regarding your use of the 100% app.")
+                    .font(.headline)
+                    .padding()
+                Text("Apple provides developers with anonymous information intended to help developers make the apps be the best apps they can be. In addition to this I've implemented Firebase Analytics (made by Google) to collect anonymous user behaviour. This helps me understand how the app is being used, is completely anonymous and can not be tracked to individual users. The Firebase Privacy Policy can be found at https://firebase.google.com/support/privacy")
+                    .font(.body)
+                    .padding()
+                Spacer()
+        
+        .navigationBarTitle(Text("Privacy Policy"))
+        }
     }
 }
 
