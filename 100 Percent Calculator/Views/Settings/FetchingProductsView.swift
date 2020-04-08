5e971259222062247b7bbeaa55254c0ad6657c9a
Thomas A Johansen <thomasaj@gmail.com>
2020-04-08 09:39:47 +0200

    Spinner to show products is being fetched - this should probably never appear in the app. D:

---
+++ 100 Percent Calculator/Views/Settings/FetchingProductsView.swift
@@ -0,0 +1,32 @@
+//
+//  FetchingProductsView.swift
+//  100 Percent Calculator
+//
+//  Created by Thomas Andre Johansen on 08/04/2020.
+//  Copyright © 2020 Appkokeriet. All rights reserved.
+//
+
+import SwiftUI
+
+struct FetchingProductsView: View {
+    @State private var animationAngle:Double = 0.0
+    var body: some View {
+        VStack(alignment: .center){
+            Image(systemName: "arrow.clockwise.circle")
+                .rotationEffect(.degrees(self.animationAngle))
+                .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: false))
+        }
+        .onAppear{
+            self.animationAngle = 360
+        }
+        .font(.largeTitle)
+        .modifier(Card(width: 150, height: 150))
+    .padding()
+    }
+}
+
+struct FetchingProductsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FetchingProductsView()
+    }
+}
